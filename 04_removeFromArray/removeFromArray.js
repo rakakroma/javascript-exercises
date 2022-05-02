8916c5bb871ab46f744f885c8fd55b124cd5ecc6
rakakroma <102795114+rakakroma@users.noreply.github.com>
2022-05-03 02:31:19 +0800

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removeElements) {
+    let removeIndices = [];
+    for (let i = 0; i < removeElements.length; i++) {
+        if (array.indexOf(removeElements[i]) > -1) {
+            removeIndices.push(array.indexOf(removeElements[i]))
+        }
+    } removeIndices = removeIndices.sort((a, b) => a - b)
+    for (let i = 0; i < removeIndices.length; i++) {
+        array.splice(removeIndices[i] - i, 1)
+    } return array
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
