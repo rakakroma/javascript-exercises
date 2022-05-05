f11b5e332e5439d329ca9294d4fa2409dc148313
rakakroma <102795114+rakakroma@users.noreply.github.com>
2022-05-06 04:52:51 +0800

    12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const date = new Date();
+    const answer = people.reduce((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = date.getFullYear();
+        }
+        if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
+            return a;
+        }; return b;
+    }); return answer
 };
 
 // Do not edit below this line
