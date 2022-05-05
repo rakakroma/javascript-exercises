0c9cb899bc840317ce3c87c24da74bfb7ebcef9c
rakakroma <102795114+rakakroma@users.noreply.github.com>
2022-05-06 04:05:30 +0800

    09 solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const azArray = string.toLowerCase().split("").filter(a => /\w/.test(a));
+    const reversedArray = [];
+    azArray.forEach(a => reversedArray.unshift(a));
+    return azArray.join("") === reversedArray.join("");
 };
-
 // Do not edit below this line
 module.exports = palindromes;
